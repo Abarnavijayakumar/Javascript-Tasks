82e73b9483e1eef80867e9132bfbb67772304c5a
Abarna <abarnavijaykumar28@gmail.com>
2025-06-03 12:02:36 +0530

    updated index.html and index.css file

---
+++ D-3 Analog Clock Task/JS/script.js
@@ -0,0 +1,9 @@
+const hours = document.querySelector('.hrs');
+const minutes = document.querySelector('.min');
+const seconds = document.querySelector('sec');
+setInterval(runClock,1000);
+
+function runclock(){
+    console.log('hello');
+    
+}
---
+++ D-3 Analog Clock Task/README.md
@@ -0,0 +1,1 @@
+# i updated this Readme file.
