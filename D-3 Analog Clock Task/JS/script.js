a319728166d48fa2790b50938467549f3f6a11a5
Abarna <abarnavijaykumar28@gmail.com>
2025-06-03 14:33:14 +0530

    updated javascript

--- D-3 Analog Clock Task/JS/script.js
+++ D-3 Analog Clock Task/JS/script.js
@@ -1,9 +1,18 @@
 const hours = document.querySelector('.hrs');
+const seconds = document.querySelector('.sec');
 const minutes = document.querySelector('.min');
-const seconds = document.querySelector('sec');
-setInterval(runClock,1000);
+setInterval(runclock,1000);
 
 function runclock(){
-    console.log('hello');
+    const time = new Date();
+    const sec = time.getSeconds()/60;
+    const min = (sec+time.getMinutes())/60;
+    const hrs = (min+time.getHours())/12;
+   
+    hours.style.setProperty('--rotation',hrs*360);
+    minutes.style.setProperty('--rotation',min*360);
+    seconds.style.setProperty('--rotation',sec*360);
+    // console.log(hrs, min, sec);
     
 }
+runclock();
